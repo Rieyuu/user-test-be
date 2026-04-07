a14629f7ec432a729f16d99672e781b036be6af1
Fathir Deny Syahputra <thetdr30@gmail.com>
2026-04-08 00:36:30 +0700

    feat: add mahasiswa module with CRUD operations and DTOs

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { MahasiswaModule } from './mahasiswa/mahasiswa.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { AuthModule } from './auth/auth.module';
     DatabaseModule,
     UsersModule,
     AuthModule,
+    MahasiswaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
